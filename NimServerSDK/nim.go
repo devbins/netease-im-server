f9ab0571af03f2bfa69a9d9dd145743d2c51fc66
binsheng <bin6160@126.com>
2018-06-22 11:21:44 +0800

    entrance

--- NimServerSDK/nim.go
+++ NimServerSDK/nim.go
@@ -4,4 +4,26 @@ type Nim struct {
 	APPKEY    string
 	APPSECRET string
 	NONCE     string
+	User      User
+	Friend    Friend
+}
+
+// New ...
+func NewNim(appkey, appsecret, nonce string) *Nim {
+	return &Nim{
+		APPKEY:    appkey,
+		APPSECRET: appsecret,
+		NONCE:     nonce,
+		User: User{
+			APPKEY:    appkey,
+			APPSECRET: appsecret,
+			NONCE:     nonce,
+		},
+		Friend: Friend{
+			APPKEY:    appkey,
+			APPSECRET: appsecret,
+			NONCE:     nonce,
+		},
+	}
+
 }
