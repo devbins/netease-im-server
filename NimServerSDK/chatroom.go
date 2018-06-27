e70828cf00c3223c03822fbfb8356d4add68c491
bin <bin6160@126.com>
2018-06-27 22:49:16 +0800

    chatroom get action

--- NimServerSDK/chatroom.go
+++ NimServerSDK/chatroom.go
@@ -13,7 +13,7 @@ type Chatroom struct {
 
 type ChatroomResult struct {
 	BaseResp
-	ChatroomInfo `json:"chatroom"`
+	ChatroomInfo ChatroomInfo `json:"chatroom"`
 }
 
 type ChatroomInfo struct {
@@ -24,6 +24,24 @@ type ChatroomInfo struct {
 	Broadcasturl string `json:"broadcasturl"`
 	Ext          string `json:"ext"`
 	Creator      string `json:"creator"`
+}
+
+type ChatroomDetailInfoResult struct {
+	BaseResp
+	ChatroomDetailInfo ChatroomDetailInfo `json:"chatroom"`
+}
+
+type ChatroomDetailInfo struct {
+	Roomid          int    `json:"roomid"`
+	Valid           bool   `json:"valid"`
+	Muted           bool   `json:"muted"`
+	Announcement    string `json:"announcement"`
+	Name            string `json:"name"`
+	Broadcasturl    string `json:"broadcasturl"`
+	Onlineusercount int    `json:"onlineusercount"`
+	Ext             string `json:"ext"`
+	Creator         string `json:"creator"`
+	Queuelevel      int    `json:"queuelevel"`
 }
 
 // Create ...
@@ -38,5 +56,20 @@ func (chatroom *Chatroom) Create(creator string, name string, announcement strin
 		return nil, err
 	}
 	return chatroomResult, nil
+
+}
+
+// Get ...
+func (chatroom *Chatroom) Get(romid int64, needOnlineUserCount bool) (*ChatroomDetailInfoResult, error) {
+	res, err := ResponseResult(chatroom.APPKEY, chatroom.APPSECRET, ACTION_CHATROOM_GET, url.Values{"roomid": {strconv.FormatInt(romid, 10)}, "needOnlineUserCount": {strconv.FormatBool(needOnlineUserCount)}})
+	if err != nil {
+		return nil, err
+	}
+	chatroomDetailInfoResult := &ChatroomDetailInfoResult{}
+	err = json.Unmarshal(res, chatroomDetailInfoResult)
+	if err != nil {
+		return nil, err
+	}
+	return chatroomDetailInfoResult, nil
 
 }
