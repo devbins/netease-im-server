7c8c34b82d92ad471968dccacf4386c0aa0f4f27
bin <bin6160@126.com>
2018-06-29 21:12:37 +0800

    refactor

--- NimServerSDK/nim.go
+++ NimServerSDK/nim.go
@@ -1,5 +1,17 @@
 package nimserversdk
 
+import (
+	"crypto/sha1"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
 type Nim struct {
 	APPKEY    string
 	APPSECRET string
@@ -32,4 +44,45 @@ func NewNim(appkey, appsecret string) *Nim {
 		},
 	}
 
+}
+
+// ResponseResult ...
+func ResponseResult(appkey string, appSecret string, action string, params url.Values) ([]byte, error) {
+	req, err := http.NewRequest("POST", action, strings.NewReader(params.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	fillHeader(req, appkey, appSecret)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return resBody, nil
+
+}
+
+// getCheckSUm ...
+func getCheckSUm(appSecret, nonce, curTime string) string {
+	sum := appSecret + nonce + curTime
+	h := sha1.New()
+	h.Write([]byte(sum))
+	result := h.Sum(nil)
+	return fmt.Sprintf("%x", result)
+}
+
+// fillHeader ...
+func fillHeader(req *http.Request, appkey, appsecret string) {
+	curTime := strconv.Itoa(int(time.Now().Unix()))
+	nonce := strconv.Itoa(rand.Int())
+	req.Header.Add("AppKey", appkey)
+	req.Header.Add("Nonce", nonce)
+	req.Header.Add("CurTime", curTime)
+	req.Header.Add("CheckSum", getCheckSUm(appsecret, nonce, curTime))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 }
